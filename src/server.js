b283c409365461f8098a0ff1e08a53c61b33d4cd
Amit <amit@AZ-iMac.local>
2018-09-30 04:07:18 -0400

    Heroku deployment troubleshooting2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/angular-app'));
