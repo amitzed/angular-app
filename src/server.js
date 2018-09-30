7bec2665cf4b82da56ecb88ad86f8ad0c3797a6a
Amit <amit@AZ-iMac.local>
2018-09-30 18:33:40 -0400

    Heroku troubleshooting cont 6

--- src/server.js
+++ src/server.js
@@ -13,4 +13,4 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/angular-app/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
