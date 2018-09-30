69fb7686f9222e618e97a8e3d1f8494922472126
Amit <amit@AZ-iMac.local>
2018-09-29 21:52:37 -0400

    Created server.js, Procfile, installed express to deploy to Heroku adjusted server.js

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
+
 // Serve static files
 app.use(express.static(__dirname + '/dist/angular-app'));
 
