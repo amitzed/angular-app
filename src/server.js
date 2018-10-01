095a4a720ebebcad8af37a25dda55a5972896e93
Amit <amit@AZ-iMac.local>
2018-10-01 19:57:41 -0400

    Heroku troubleshooting cont 48

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/dist/angular-app'));
 
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
+// app.get('/*', function(req,res) {
+//
+// res.sendFile(path.join(__dirname+'/dist/angular-app/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3001);
