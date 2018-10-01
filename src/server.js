899aadc5d7cfb3e39141c678d913cbc4a4a62345
Amit <amit@AZ-iMac.local>
2018-10-01 19:47:24 -0400

    Heroku troubleshooting cont 46

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-app'));
 
 app.get('/*', function(req,res) {
-
 res.sendFile(path.join(__dirname+'/dist/angular-app/index.html'));
 });
 
