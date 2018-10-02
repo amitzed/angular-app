f84092cb089f953c045c7747ac7e4affff26918e
Amit <amit@AZ-iMac.local>
2018-10-01 20:03:31 -0400

    Heroku troubleshooting cont 49

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-app'));
+app.use(express.static(__dirname + '/dist'));
 
 // app.get('/*', function(req,res) {
 //
